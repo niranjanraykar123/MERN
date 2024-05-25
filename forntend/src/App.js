6a2ff5d36be5391b3d9acf71321df71dcb9879e3
niranjan123 <raykarniranjan@gmail.com>
2024-05-25 12:53:44 +0530

    changes made

--- forntend/src/App.js
+++ forntend/src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import Post from "./post";
+import Header from "./header";
+import {Route,Routes} from "react-router-dom"
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">
-          Myblog
-        </a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
+    <Routes>
+      <Route index element={
+        <main>
+        <Header />
          <Post />
          <Post />
          <Post />
 
     </main>
+      }>
+
+      </Route>
+
+      <Route path={'/login'} element={
+        <div>login</div>
+      }></Route>
+      <Route path={'/register'} element={
+        <div>Register</div>
+      }></Route>
+    </Routes>
+    
   );
 }
-
 export default App;
